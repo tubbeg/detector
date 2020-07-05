bcba8a8060db03a0284866af8d8cc3dcb3b3bdc1
Tobias Gärdebro <tubbeg@gmail.com>
2020-07-05 18:27:59 +0200

    Figured out why resizing didn't work. The images in mnist are grayscale so I changed (_, 50, 50, 1) to (_, 50, 50, 3). Also hade to change this when adding layers in method __create_model.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
 from tensorflow import keras
+import cv2
+import numpy as np
 
 """
 CIFAR-10 dataset consists of 60000 32x32 colour images in 10 classes, with 6000 images per class.
@@ -20,13 +22,20 @@ class Detector:
         self.y_train = None
         self.x_test = None
         self.y_test = None
-        self.res = 32
+        self.res = 50
+        self.output_number = 2
+        self.index_size = 6
+        self.dimensions = 3  # set to 1 for grayscale images
 
     def load_data(self, x_train, y_train, x_test, y_test):
-        (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+        # X_train, y_train), (X_test, y_test) = mnist.load_data()
         # reshape data to fit model
-        X_train = X_train.reshape(50000, self.res, self.res, 1)
-        X_test = X_test.reshape(10000, self.res, self.res, 1)
+        X_train = x_train
+        X_test = x_test
+
+
+        X_train = X_train.reshape(self.index_size, self.res, self.res, self.dimensions)
+        X_test = X_test.reshape(self.index_size, self.res, self.res, self.dimensions)
 
         # one-hot encode target column
         y_train = to_categorical(y_train)
@@ -48,10 +57,10 @@ class Detector:
 
     def __create_model(self):
         self.model = Sequential()  # add model layers
-        self.model.add(Conv2D(64, kernel_size=3, activation="relu", input_shape=(self.res, self.res, 1)))
+        self.model.add(Conv2D(64, kernel_size=3, activation="relu", input_shape=(self.res, self.res, 3)))
         self.model.add(Conv2D(32, kernel_size=3, activation='relu'))
         self.model.add(Flatten())
-        self.model.add(Dense(10, activation='softmax'))
+        self.model.add(Dense(self.output_number, activation='softmax'))
 
     def __compile_model(self):
         self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -66,8 +75,8 @@ class Detector:
             raise Exception("model None!")
         self.model = keras.models.load_model(model)
 
-    def predict(self):
-        return self.model.predict(self.x_test[:4])
+    def predict(self, image_array):
+        return self.model.predict(image_array)
 
 
 def test_detector():
@@ -75,17 +84,42 @@ def test_detector():
     # real interesting guide https://github.com/jerett/Keras-CIFAR10/blob/master/softmax.ipynb
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
     detector.load_data(X_train, y_train, X_test, y_train)
-    #detector.build_model()
-    #detector.train_model()
-    #detector.save_model("./my_model")
-    detector.load_model("./my_model")
+    detector.build_model()
+    detector.train_model()
+    detector.save_model("./my_model2")
+    #detector.load_model("./my_model")
     print(detector.predict())
 
 
+def test_detector2():
+    detector = Detector('./')
+    # real interesting guide https://github.com/jerett/Keras-CIFAR10/blob/master/softmax.ipynb
+    #(X_train, y_train), (X_test, y_test) = mnist.load_data()
+    y_train = [0, 1, 0, 1, 0, 1]
+    y_test = y_train
+    im1 = cv2.imread("square.jpg")
+    im2 = cv2.imread("circle.jpg")
+    X_train = np.array([im1, im2, im1, im2, im1, im2])
+    X_test = X_train
+
+    detector.load_data(X_train, y_train, X_test, y_train)
+    detector.build_model()
+    detector.train_model()
+    detector.save_model("./my_model2")
+    #detector.load_model("./my_model")
+    print(detector.predict(X_train[:6]))
+
+
 def main():
-    test_detector()
+    test_detector2()
 
 
 if __name__ == '__main__':
-    # main2()
-    test_detector()
+    main()
+    #(X_train, y_train), (X_test, y_test) = mnist.load_data()
+    #plt.imshow(X_train[1])
+    #print(type(X_train))
+    #plt.show()
+
+    #plt.imshow()
+    #test_detector()
